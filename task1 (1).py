5a73fda4a8d7b225db273e68501af2978ed54da0
ADITYA <aditya11surve08@gmail.com>
2024-09-28 15:17:35 +0530

    jp morgan internship

---
+++ task1 (1).py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def exercise_0(file):
+    return pd.read_csv(file)
+
+def exercise_1(df):
+    return list(df)
+
+def exercise_2(df, k):
+    return df.head(k)
+
+def exercise_3(df, k):
+    return df.sample(n=k)
+
+def exercise_4(df):
+    return df['type'].unique()
+
+def exercise_5(df):
+    return df['nameDest'].value_counts().head(10)
+
+def exercise_6(df):
+    pass
+
+def exercise_7(df):
+    pass
+
+def visual_1(df):
+    pass
+
+def visual_2(df):
+    pass
+
+def exercise_custom(df):
+    pass
+    
+def visual_custom(df):
+    pass
